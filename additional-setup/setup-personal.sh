3248b031e5be9aa235d59814deb13645eafbabb1
Lucas Martins de Oliveira <lucasm236954@gmail.com>
2025-01-29 00:19:28 -0300

    feat: add asdf manager installation
    Adicionando instalação do asdf manager para gestão de versão para
    linguagens de programação. Instalando plugins para NodeJS e Java.
    
    Changes to be committed:
    	modified:   setup.sh

--- additional-setup/setup-personal.sh
+++ additional-setup/setup-personal.sh
@@ -1,16 +1,18 @@
 #!/bin/bash
 
-# apps de sistema
+# --------------- INSTALAR APPS DE TERMINAL ---------------
 print_status "instalando apps de sistema..."
 sudo dnf install -y \
     qbittorrent \
     steam
 check_status "instalar apps de sistema"
+# ---------------
 
-# instalar apps flatpak
+# --------------- INSTALAR APPS FLATPAK ---------------
 print_status "instalando apps flatpak..."
 flatpak install -y flathub \
     com.discordapp.discord \
     com.heroicgameslauncher.hgl \
     com.parsecgaming.parsec
 check_status "instalar apps flatpak"
+# ---------------
--- additional-setup/setup-work.sh
+++ additional-setup/setup-work.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 
-# instalar apps flatpak
+# --------------- INSTALAR APPS FLATPAK ---------------
 print_status "instalando apps flatpak..."
 flatpak install -y flathub \
     io.dbeaver.dbeavercommunity
 check_status "instalar apps flatpak"
+# ---------------
