01442abdb8bcff154651fedb3939f240a555da6d
Lucas Martins de Oliveira <lucastmartins.dev@gmail.com>
2025-06-22 00:57:49 -0300

    [fix] fixing rpmfusion config

--- setup.sh
+++ setup.sh
@@ -35,7 +35,7 @@ check_status "atualizar o sistema"
 
 # --------------- HABILITAR RPM FUSION ---------------
 print_status "habilitando rpm fusion..."
-sudo dnf install -y https://mirrors.rpmfusion.org/free/fedora/rpmfusion-free-release-$(rpm -e %fedora).noarch.rpm https://mirrors.rpmfusion.org/nonfree/fedora/rpmfusion-nonfree-release-$(rpm -e %fedora).noarch.rpm
+sudo dnf install -y https://mirrors.rpmfusion.org/free/fedora/rpmfusion-free-release-$(rpm -E %fedora).noarch.rpm https://mirrors.rpmfusion.org/nonfree/fedora/rpmfusion-nonfree-release-$(rpm -E %fedora).noarch.rpm
 check_status "habilitar rpm fusion"
 # ---------------
 
